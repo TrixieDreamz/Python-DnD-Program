0f93e241484d89eb09328e8e9bd8db0beeaf2545
TrixieDreamz <146946340+TrixieDreamz@users.noreply.github.com>
2024-03-21 07:25:36 +0100

    ""

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 print("Hello World")
 print("GitHub App Test")
+
+# Things I want to add to my program
+"""
+Stat Generator--
+Race Generator--
+Class Generator--
+Diety Generator--
+Name Generator--
+Item Generator--
+Treasure Generator--
+
+
+"""
